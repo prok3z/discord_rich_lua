204cb24d859ba55da986881493ca5b48c2f8361a
Dock <43358190+yurigabriel4@users.noreply.github.com>
2020-05-14 19:55:57 -0300

    discord_rich.lua
    Progama para criar seu discord rich

---
+++ discord_rich.lua
@@ -0,0 +1,16 @@
+  
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(10000)
+        players = {}
+        for i = 0,256 do
+            if NetworkIsPlayerActive(i) then
+                table.insert(players,i)
+            end
+        end
+        SetDiscordAppId(710290194489933857) -- ID DO APP AQUI			
+		SetDiscordRichPresenceAssetText('DESCRIÇÃO AQUI') -- PNG DESCRIÇÃO 1 TEXTO
+        SetDiscordRichPresenceAsset('pycharm512')
+        SetRichPresence("Cidadões Presentes: "..#players)
+    end	
+end)
